5ed4405220ba143d8735fa03d17d18eeb23c4bfe
leonardo15043 <lahernandez15043@gmail.com>
2019-02-14 21:30:47 -0500

    Mostrar información de Firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,16 +2,25 @@ import { Component } from '@angular/core';
 import { ModalController } from 'ionic-angular';
 import { SubirPage  } from '../subir/subir';
 
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    
+  items: Observable<any[]>;
 
-  constructor( private modalCtr:ModalController ) {
+  constructor( 
+    private modalCtr:ModalController, afDB: AngularFireDatabase
+     ) {
+      this.items = afDB.list('post').valueChanges();
 
-  }
+      console.log(this.items);
+     }
 
   mostrar_model(){
 
